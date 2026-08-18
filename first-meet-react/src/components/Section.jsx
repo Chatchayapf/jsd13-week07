10f77a9c1a60eb8ec2952b62d71e2fdb1e99ef7c
Chatchayapf <chatchaya.pf@gmai.com>
2026-08-18 22:32:06 +0700

    updatepic

--- first-meet-react/src/components/Section.jsx
+++ first-meet-react/src/components/Section.jsx
@@ -1,13 +1,22 @@
-export default function Data () {
-    return (        
-    <section className="w-full p-5 bg-amber-100 flex">
-          <ul className="list-inside list-disc flex-1">
-            <span className="font-semibold">Tech Stack:</span>
-            <li>Vite</li>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Tailwind</li>
-          </ul>
-        </section>
-    );
+import pompompurin from "../assets/pompompurin.jpg"
+
+export default function Section() {
+  return (
+    <section className=" gap-4 p-5 bg-amber-100 flex ">
+      <ul className="list-inside list-disc flex-1">
+        <span className="font-semibold">IMG responsive:</span>
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <img src={pompompurin}/>
+          <img src={pompompurin}/>
+          <img src={pompompurin}/>
+          <img src={pompompurin}/>
+          <img src={pompompurin}/>
+          <img src={pompompurin}/>
+          
+          
+        </div>
+      </ul>
+    </section>
+  );
 }
